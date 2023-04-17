5ea5704bee2d1eadefb4beb381288252d51fe9df
Ifeoluwa Odewale <ifeoluwa.odewale@gmail.com>
2023-04-17 02:24:41 +0100

    Feature: scrape login process

--- src/scrapers/index.ts
+++ src/scrapers/index.ts
@@ -2,17 +2,46 @@ import puppeteer from "puppeteer";
 
 const scrapePages = async ()  => {
     const browser = await puppeteer.launch({
-        headless: process.env.NODE_ENV !== 'production',
+        headless: process.env.NODE_ENV === 'production',
         defaultViewport: null
     });
 
     const page = await browser.newPage();
 
+    page.on("dialog", async (dialog) => {
+        await dialog.accept();
+    })
+
     // TODO: Create auth object after initial login
+    await page.goto('https://bankof.okra.ng/', {
+        waitUntil: "domcontentloaded"
+    });
+    await page.click("nav > a.bg-black")
+    await page.waitForSelector("#email")
+
+    const auth = {
+        email: "ifeoluwa.odewale@gmail.com",
+        password: "Ifeola@0411",
+        otp: "12345"
+    }
+
+    await page.type("#email", auth.email)
+    await page.type("#password", auth.password)
+    await page.click("button[type=submit]")
+
+    await page.waitForSelector("#otp");
+
+    await page.type("#otp", auth.otp);
+
+    await Promise.all([
+        page.click("button[type=submit]"),
+        page.waitForNavigation()
+    ]);
+
     // TODO: Create customer obhect
     // TODO: Create account object
     // TODO: Create transaction object
-    await browser.close();
+    // await browser.close();
 }
 
 
